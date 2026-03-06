86e285fcc1b3e233f81136537e1cc8ab1c765479
fengzhe034 <dujiazhuoyu@gmail.com>
2026-03-06 22:57:44 +0800

    feat: 完成前后端初始化

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// 把里面的内容改成：
+function App() {
+  return (
+    <div>
+      <h1>情绪日记项目</h1>
+      <p>我是前端组：[Djzy]</p>
+      <p>项目已成功运行！</p>
+    </div>
+  )
+}
+
+export default App
